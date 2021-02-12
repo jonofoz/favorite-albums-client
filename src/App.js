22bb468eaf3b7f0e31cf362e6fa01f1bc4b97f30
JonOfOz <jon@jonofoz.com>
2021-02-12 09:52:25 -0600

    Add Apollo stuff to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
 import AlbumList from "./components/AlbumList";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
 
 function App() {
   return (
-    <div className="App">
-      <h1>JonOfOz's Favorite Albums, Like Ever</h1>
-      <AlbumList />
-    </div>
+    <apolloProvider client={client}>
+      <div id="main">
+        <h1>JonOfOz's Favorite Albums, Like Ever</h1>
+        <AlbumList />
+      </div>
+    </apolloProvider>
   );
 }
 
