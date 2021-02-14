04ad2954a92cb8b07f698ce20eabe642990f9b46
JonOfOz <jon@jonofoz.com>
2021-02-14 11:01:09 -0600

    Continue work on AddAlbumForm

--- src/components/AddAlbumForm.js
+++ src/components/AddAlbumForm.js
@@ -1,33 +1,37 @@
 import { useState } from 'react'
 
 const AddAlbumForm = props => {
-    const [name, setName] = useState('');
-    const [artistId, setArtistId] = useState('');
+    const [album, setAlbum] = useState('');
+    const [artist, setArtist] = useState('');
     const [ranking, setRanking] = useState('');
     const [genre, setGenre] = useState(['']);
     const [YOR, setYOR] = useState(''); // Year Of Release
     const [commentary, setCommentary] = useState('');
 
-    const onFormSubmit = e => {
-        // e.preventDefault;
-        // console.log(e);
+    const onFormSubmit = (e, props) => {
+        e.preventDefault();
+        console.log(album);
+        console.log(artist);
+        console.log(ranking);
+        console.log(YOR);
+        console.log(commentary);
+        debugger;
     };
 
     return (
         <div className="container-add-album">
-            <form onSumbit={(e) => onFormSubmit(e)}>
-                <label for="add-album-name">Name</label>
-                <input id="add-album-name" value={name} onChange={(e) => setName(e.target.value)}></input>
-                <label for="add-album-artistId">Artist ID</label>
-                <input id="add-album-artistId" value={artistId} onChange={(e) => setArtistId(e.target.value)}></input>
-                <label for="add-album-ranking">Ranking</label>
-                <input id="add-album-ranking" value={ranking} onChange={(e) => setRanking(e.target.value)}></input>
-                {/* TODO: Handle adding genres. */}
-                {/* <input value={genre}></input> */}
-                <label for="add-album-YOR">Year Of Release</label>
-                <input id="add-album-YOR" value={YOR} onChange={(e) => setYOR(e.target.value)} placeholder="Year of Release"></input>
-                <label for="add-album-commentary">Commentary</label>
-                <input id="add-album-commentary" value={commentary} onChange={(e) => setCommentary(e.target.value)}></input>
+            <form id="form-add-album" onSubmit={(e) => onFormSubmit(e, props)}>
+                <label className="label-add-album" htmlFor="add-album-name">Album</label>
+                <input type="text" id="add-album-album" value={album} onChange={(e) => setAlbum(e.target.value)}></input>
+                <label className="label-add-album" htmlFor="add-album-artist">Artist</label>
+                <input type="text" id="add-album-artist" value={artist} onChange={(e) => setArtist(e.target.value)}></input>
+                <label className="label-add-album" htmlFor="add-album-ranking">Ranking</label>
+                <input type="text" id="add-album-ranking" value={ranking} onChange={(e) => setRanking(e.target.value)}></input>
+                <label className="label-add-album" htmlFor="add-album-YOR">Year Of Release</label>
+                <input type="text" id="add-album-YOR" value={YOR} onChange={(e) => setYOR(e.target.value)}></input>
+                <label className="label-add-album" htmlFor="add-album-commentary">Commentary</label>
+                <input type="text" id="add-album-commentary" value={commentary} onChange={(e) => setCommentary(e.target.value)}></input>
+                <button>+</button>
             </form>
         </div>
     )
