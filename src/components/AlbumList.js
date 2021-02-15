03c9918c5d42b292ea2e65bb0377d8d7ba6bb8e1
JonOfOz <jon@jonofoz.com>
2021-02-14 19:40:38 -0600

    Implement drag 'n' drop re-order functionality (no saving yet)

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,19 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { graphql } from 'react-apollo';
 import { getAlbumsQuery } from '../queries/queries';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 import Album from './Album';
 
 const AlbumList = (props) => {
     const [selected, setSelected] = useState('');
+    const [albums, setAlbums] = useState([]);
+
+    useEffect(() => {
+        if (!props.data.loading && props.data.albums) {
+            setAlbums(props.data.albums);
+        }
+    })
 
     // Changes which details to show, or hides if user clicks on album with details already shown
     const checkSelected = (id) => {
@@ -18,34 +26,61 @@ const AlbumList = (props) => {
     }
 
     const populateAlbums = () => {
-        if (props.data.loading || !props.data.albums) {
+        if (props.data.loading || !albums.length) {
             return (<li>Loading albums...</li>)
         }
         else {
-            const albums = props.data.albums;
             // Sort the albums by ranking before displaying
-
             albums.sort((a, b) => (a.ranking > b.ranking) ? 1 : -1);
             return (
-                albums.map(album => {
+                albums.map((album, index)=> {
                     return (
-                        <li key={album.id} onClick={() => checkSelected(album.id)}>
-                            <Album
-                                album={album}
-                                showDetails={selected === album.id ? true : false}
-                            />
-                        </li>
+                        <Draggable key={album.id} draggableId={album.id} index={index}>
+                            {(provided) => (
+                                <li onClick={() => checkSelected(album.id)} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                                    <Album
+                                        album={album}
+                                        showDetails={selected === album.id ? true : false}
+                                    />
+                                </li>
+                            )}
+                        </Draggable>
                     )
                 })
             )
 
         }
     }
+
+    const onDragEnd = result => {
+        var {
+            draggableId,
+            source: { index: srcIndex },
+            destination: { index: destIndex }
+        } = result;
+        if (srcIndex != destIndex) {
+            const draggedAlbum = albums.filter(a => a.id === draggableId)[0];
+            const updatedAlbums = Array.from(albums);
+            updatedAlbums.splice(srcIndex, 1)
+            updatedAlbums.splice(destIndex, 0, draggedAlbum)
+            updatedAlbums.forEach((a, index) => {
+                a.ranking = index + 1;
+            })
+            setAlbums(updatedAlbums);
+        }
+    }
     return (
         <div id="album-list-container">
-            <ol>
-                {populateAlbums()}
-            </ol>
+            <DragDropContext onDragEnd={onDragEnd}>
+                <Droppable droppableId="albums">
+                    {(provided) => (
+                        <ol {...provided.droppableProps} ref={provided.innerRef}>
+                            {populateAlbums()}
+                            {provided.placeholder}
+                        </ol>
+                    )}
+                </Droppable>
+            </DragDropContext>
         </div>
     )
 }
